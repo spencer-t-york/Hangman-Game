32ba481254dbf4f794ab1f7140f54978d7cd4c4f
Barry-the-Bagel <80482507+Barry-the-Bagel@users.noreply.github.com>
2022-10-15 12:25:08 -0400

    Add files via upload

--- CIS260_York_Homework7_Question2.java
+++ CIS260_York_Homework7_Question2.java
@@ -20,93 +20,112 @@ import java.util.Scanner;
 public class CIS260_York_Homework7_Question2 {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("");                 // Spacing for terminal visibility
+        boolean game = true;
 
-        // ------------------------------------------- RANDOM WORD GENERATOR ------------------------------------------- //
-        String[] words = new String[6801];
-
-        // Create Word List
-        URL text = new URL("http://www.desiquintans.com/downloads/nounlist/nounlist.txt");
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(text.openStream()));
-
-        String inputLine;
-        for (int i = 0; (inputLine = in.readLine()) != null; i++) {
-            words[i] = inputLine;
-        }
-        in.close();
-
-        // Choose word from list
-        Random random = new Random();
-        int i = random.nextInt(words.length);
-        String word = words[i];
-        System.out.println(word);
-
-        // VARIABLE | String word | IS THE RETURN VARIABLE
-
-        // SPLIT WORDS INTO CHARCTERS (using an array and charAt())
-        String[] splitWord = new String[word.length()];
-
-        for (i = 0; i < word.length(); i++) 
+        while (game)
         {
-            splitWord[i] = Character.toString(word.charAt(i));
-        }
-        System.out.println("");                 //Spacing for terminal visibility
+            // ------------------------------------------- RANDOM WORD GENERATOR ------------------------------------------- //
+            String[] words = new String[6801];
 
-        
-        // ------------------------------------------- HIDE WORD FROM USER ------------------------------------------- //
-        // Use asteriks
-        String[] coveredSplitWord = new String[word.length()];
+            // Create Word List
+            URL text = new URL("http://www.desiquintans.com/downloads/nounlist/nounlist.txt");
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(text.openStream()));
 
-        for (i = 0; i < word.length(); i++) 
-        {
-            coveredSplitWord[i] = Character.toString(word.charAt(i));
-            coveredSplitWord[i] = "*";
-            System.out.print(coveredSplitWord[i]);
-        }
-        System.out.println("");
-
-        // ------------------------------------------- USER PROMPTS ------------------------------------------- //
-
-        int counter = 0;
-        
-        // scanner
-        Scanner input = new Scanner(System.in);
-
-        // loop
-        while(true)
-        {
-            // USER PROMPT
-            System.out.print("\nGuess a letter in the word: ");
-            String userInput = input.next();
-
-            if (Arrays.asList(coveredSplitWord).contains(userInput)) {
-                // TODO logo and continue if true
-                System.out.print("Word already guessed correctly");
-                continue;
+            String inputLine;
+            for (int i = 0; (inputLine = in.readLine()) != null; i++) {
+                words[i] = inputLine;
             }
+            in.close();
 
-            if (!word.contains(userInput)) {
-                
-                counter++;
-                System.out.print(userInput + " is not in the word.");
-            }
-            System.out.println(coveredSplitWord);
+            // Choose word from list
+            Random random = new Random();
+            int i = random.nextInt(words.length);
+            String word = words[i];
 
-            // nested Loop
-            for (int j = 0; j < word.length(); j++) 
+            // VARIABLE | String word | IS THE RETURN VARIABLE
+
+            // SPLIT WORDS INTO CHARCTERS (using an array and charAt())
+            String[] splitWord = new String[word.length()];
+
+            for (i = 0; i < word.length(); i++) 
             {
-                // if guess is correct
-                if (Objects.equals(userInput, splitWord[j]))
-                {
-                    // uncover letter
-                    coveredSplitWord[j] = userInput;
-                }
+                splitWord[i] = Character.toString(word.charAt(i));
             }
+            System.out.println("");                 //Spacing for terminal visibility
 
-            if (!Arrays.asList(coveredSplitWord).contains("*")) {
-                System.out.println("\n\nYou guessed the word!\n Y/N");
-                break;  
+            
+            // ------------------------------------------- HIDE WORD FROM USER ------------------------------------------- //
+            // Use asteriks
+            String[] coveredSplitWord = new String[word.length()];
+
+            for (i = 0; i < word.length(); i++) 
+            {
+                coveredSplitWord[i] = Character.toString(word.charAt(i));
+                coveredSplitWord[i] = "*";
+                System.out.print(coveredSplitWord[i]);
+            }
+            System.out.println("");
+
+            // ------------------------------------------- USER PROMPTS ------------------------------------------- //
+            int counter = 0;
+            boolean play = true;
+            
+            // scanner
+            Scanner input = new Scanner(System.in);
+
+            // while play == true
+            while(play)
+            {
+                // USER PROMPT
+                System.out.print("\nGuess a letter in the word: ");
+                String userInput = input.next();
+
+                if (Arrays.asList(coveredSplitWord).contains(userInput)) {
+                    // TODO logo and continue if true
+                    System.out.print("Letter already guessed correctly\n");
+                    continue;
+                }
+
+                if (!word.contains(userInput)) {
+                    counter++;
+                    System.out.print(userInput + " is not in the word\n");
+                }
+
+                // nested Loop
+                for (int j = 0; j < word.length(); j++) 
+                {
+                    // if guess is correct
+                    if (Objects.equals(userInput, splitWord[j]))
+                    {
+                        // uncover letter
+                        coveredSplitWord[j] = userInput;
+                    }
+                    System.out.print(coveredSplitWord[j]);
+                }
+                System.out.println("");
+
+                if (!Arrays.asList(coveredSplitWord).contains("*")) {
+                    System.out.println("\n\nYou guessed the word!\n");
+                    System.out.println("You missed " + counter + " times.\n");
+                    System.out.println("Would you like to play again Y/N?");
+                    userInput = input.next();
+                    if ("Y".equals(userInput))
+                    {
+                        game = true;
+                        play = false;
+                    }
+                    else if ("N".equals(userInput))
+                    {
+                        play = false;
+                        game = false;
+                    }
+                    else
+                    {
+                        play = false;
+                        game = false;
+                    }
+                }
             }
         }
     }
@@ -137,36 +156,3 @@ public class CIS260_York_Homework7_Question2 {
  * |    Do you want to guess another word? Enter y or n    |
  * |–———————————————————————————————————————————————–––––––|
  */
-
-// Just in case...
- /*
-  *        Scanner input = new Scanner(System.in);
-
-        for (i = 0; i < word.length(); i++)
-        {
-            System.out.print("\nGuess a letter in the word: ");
-            String userInput = input.next();
-
-            for (int j = 0; j < word.length(); j++) 
-            {
-                if (!Objects.equals(userInput, splitWord[j])) 
-                {
-                    wrongGuess++;
-                }
-                else
-                {
-                    for (int k = 0; k < word.length(); k++) 
-                    {
-                        if (Objects.equals(userInput, splitWord[k]))
-                        {
-                            coveredSplitWord[k] = userInput;
-                        }
-                    }
-                }
-                System.out.print(coveredSplitWord[j]);
-            }
-        }
-        System.out.println("");
-    }
-}
-  */
